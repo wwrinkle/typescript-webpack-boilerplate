4ee421e0d2b4be4d305f670be903852f10642d5b
Willie Wrinkle <williewrinkle@gmail.com>
2019-03-13 22:10:17 -0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: path.join(__dirname),
+    compress: true,
+    port: 8080,
+    watchContentBase: true,
+    publicPath: '/dist/'
+  }
+};
